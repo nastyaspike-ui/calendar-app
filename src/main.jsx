253ab46f3570bf0c2bae17fab6616f34ed699381
nastyaspike-ui <nastyaspike@gmail.com>
2026-06-02 17:17:50 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './Dashboard.jsx'
+import CalendarPage from './CalendarPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/cal/:calId" element={<CalendarPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
